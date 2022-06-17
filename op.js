99656529fc23f4c24eb602f167429b9cfc1ba8ef
monomiki <107701198+monomiki@users.noreply.github.com>
2022-06-18 00:22:35 +0900

    Add files via upload

---
+++ op.js
@@ -0,0 +1,22 @@
+(() => {
+
+  //OPアニメーション
+
+  window.addEventListener('DOMContentLoaded', () => {
+
+    const body = document.body;
+    const opLogo = document.querySelector("#opLogo");
+    const content = document.querySelector("#content");
+
+    window.setTimeout(function(){
+      opLogo.classList.add('none');
+      body.classList.add('slideIn');
+    },1000);
+
+    window.setTimeout(function () {
+      content.classList.add('appear');
+    }, 1400);
+
+  });
+
+})();
